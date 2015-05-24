2fc1a93d51283877bcc73679e0798bff5a8491fd
Bruno Daniel <bruno@vault7.com>
2015-05-24 13:47:57 -0300

    Circle can now generate open arcs from an angle

--- Primitives/Circle.cs
+++ Primitives/Circle.cs
@@ -9,63 +9,61 @@ namespace Forge.Primitives {
 		public bool Filled = false;
 		public Vector3 Center = Vector3.zero;
 		public float Radius = .5f;
+		public float Angle = 360;
 
 		public Geometry Output() {
-			int vertexCount = Filled ? Segments + 1 : Segments;
-			int triCount = Filled ? Segments * 3 : 0;
+			Segments = Segments + (Angle < 360 ? 1 : 0);
+			int vertexCount = Segments + (Filled ? 1 : 0);
+
 			Geometry geo = new Geometry();
 			geo.Vertices = new Vector3[vertexCount];
-			geo.Normals = new Vector3[vertexCount];
 			geo.UV = new Vector2[vertexCount];
-			geo.Triangles = new int[triCount];
+			geo.Triangles = new int[Segments * 3 - (Angle < 360 ? 3 : 0)];
 
-			float radiansInACircle = Mathf.PI * 2;
+			float radians = Angle * Mathf.Deg2Rad;
 
 			for (int i = 0; i < Segments; i++) {
-				float angle = radiansInACircle * i / Segments;
+				int offset = Angle < 360 ? 1 : 0;
+				float angle = radians * i / (Segments - offset);
 				float cos = Mathf.Cos(angle) * Radius;
 				float sin = Mathf.Sin(angle) * Radius;
 
 				switch (OrientationPlane) {
 					case OrientationPlane.XZ:
 						geo.Vertices[i] = new Vector3(Center.x + cos, Center.y, Center.z + sin);
-						geo.Normals[i] = Vector3.up;
 						break;
 					case OrientationPlane.YZ:
-						geo.Vertices[i] = new Vector3(Center.x, Center.y + cos, Center.z + sin);
-						geo.Normals[i] = Vector3.right;
+						geo.Vertices[i] = new Vector3(Center.x, Center.y + sin, Center.z + cos);
 						break;
 					case OrientationPlane.XY:
 						geo.Vertices[i] = new Vector3(Center.x + cos, Center.y + sin, Center.z);
-						geo.Normals[i] = Vector3.forward;
 						break;
 				}
 
 				geo.UV[i] = new Vector2((i % 2 == 0) ? 0f : 1f, 0f);
 				
-				if (Filled && OrientationPlane == OrientationPlane.XZ) {
-					geo.Triangles[i * 3] = i;
-					geo.Triangles[i * 3 + 1] = Segments;
-					geo.Triangles[i * 3 + 2] = (i == Segments - 1) ? 0 : i + 1;
-				} else if (Filled) {
-					geo.Triangles[i * 3] = i;
-					geo.Triangles[i * 3 + 1] = (i == Segments - 1) ? 0 : i + 1;
-					geo.Triangles[i * 3 + 2] = Segments;
-				}
 			}
 
 			if (Filled) {
+				for (int i = 0; i < Segments - (Angle < 360 ? 1 : 0); i++) {
+					if (OrientationPlane == OrientationPlane.XZ) {
+						geo.Triangles[i*3  ] = i;
+						geo.Triangles[i*3+1] = Segments;
+						geo.Triangles[i*3+2] = (i == Segments - 1) ? 0 : i + 1;
+					}
+					else {
+						geo.Triangles[i*3  ] = i;
+						geo.Triangles[i*3+1] = (i == Segments - 1) ? 0 : i + 1;
+						geo.Triangles[i*3+2] = Segments;
+					}
+				}
+
 				geo.Vertices[Segments] = Center;
 				geo.UV[Segments] = new Vector2(.5f, 1f);
-				switch (OrientationPlane) {
-					case OrientationPlane.XZ:
-						geo.Normals[Segments] = Vector3.up; break;
-					case OrientationPlane.YZ:
-						geo.Normals[Segments] = Vector3.right; break;
-					case OrientationPlane.XY:
-						geo.Normals[Segments] = Vector3.forward; break;
-				}
+				
 			}
+
+			geo.CalculateNormals();
 
 			return geo;
 		}
