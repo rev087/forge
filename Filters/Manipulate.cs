fb2d627900e3e27da9be9b1ab78d8fd7b8a7428b
Bruno Daniel <bruno@vault7.com>
2015-05-25 17:56:16 -0300

    Manipulate.Output() is now properly idempotent

--- Filters/Manipulate.cs
+++ Filters/Manipulate.cs
@@ -21,28 +21,29 @@ namespace Forge.Filters {
 		}
 
 		public Geometry Output() {
+			Geometry geo = _geometry.Copy();
 
 			if (Position != Vector3.zero) {
-				for (int i = 0; i < _geometry.Vertices.Length; i++) {
-					_geometry.Vertices[i] = _geometry.Vertices[i] + Position;
+				for (int i = 0; i < geo.Vertices.Length; i++) {
+					geo.Vertices[i] = geo.Vertices[i] + Position;
 				}
 			}
 
 			if (Rotation != Vector3.zero) {
 				Quaternion qRot = Quaternion.Euler(Rotation);
-				for (int i = 0; i < _geometry.Vertices.Length; i++) {
-					_geometry.Vertices[i] = qRot * _geometry.Vertices[i];
-					_geometry.Normals[i] = qRot * _geometry.Normals[i];
+				for (int i = 0; i < geo.Vertices.Length; i++) {
+					geo.Vertices[i] = qRot * geo.Vertices[i];
+					geo.Normals[i] = qRot * geo.Normals[i];
 				}
 			}
 
 			if (Scale != Vector3.one) {
-				for (int i = 0; i < _geometry.Vertices.Length; i++) {
-					_geometry.Vertices[i] = Vector3.Scale(_geometry.Vertices[i], Scale);
+				for (int i = 0; i < geo.Vertices.Length; i++) {
+					geo.Vertices[i] = Vector3.Scale(geo.Vertices[i], Scale);
 				}
 			}
 
-			return _geometry;
+			return geo;
 		}
 
 
