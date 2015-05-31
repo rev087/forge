77030334740993bcc51b28068e563574d0a78377
Bruno Daniel <bruno@vault7.com>
2015-05-31 18:39:16 -0300

    Mirror node output is now properly idempotent

--- Filters/Mirror.cs
+++ Filters/Mirror.cs
@@ -21,23 +21,30 @@ namespace Forge.Filters {
 
 		public Geometry Output() {
 
+			Geometry geo = new Geometry() {
+				Vertices = new Vector3[_geometry.Vertices.Length],
+				Normals = new Vector3[_geometry.Normals.Length],
+				UV = new Vector2[_geometry.UV.Length],
+				Triangles = new int[_geometry.Triangles.Length]
+			};
+
 			for (int i = 0; i < _geometry.Vertices.Length; i++) {
 				Vector3 v = _geometry.Vertices[i];
 
 				switch (Axis) {
 					case Axis.X:
-						_geometry.Vertices[i] = new Vector3(-v.x, v.y, v.z);
+						geo.Vertices[i] = new Vector3(-v.x, v.y, v.z);
 						break;
 					case Axis.Y:
-						_geometry.Vertices[i] = new Vector3(v.x, -v.y, v.z);
+						geo.Vertices[i] = new Vector3(v.x, -v.y, v.z);
 						break;
 					case Axis.Z:
-						_geometry.Vertices[i] = new Vector3(v.x, v.y, -v.z);
+						geo.Vertices[i] = new Vector3(v.x, v.y, -v.z);
 						break;
 				}
 			}
 			
-			return Reverse.Process(_geometry);
+			return geo;
 		}
 
 		public static Geometry Process(Geometry geometry) {
