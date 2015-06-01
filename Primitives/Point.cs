5d9ea67d19c5338c660acf08672715749e9bd24d
Bruno Daniel <bruno@vault7.com>
2015-05-31 23:09:15 -0300

    Convenience methods for Point primitive

--- Primitives/Point.cs
+++ Primitives/Point.cs
@@ -6,6 +6,22 @@ namespace Forge {
 
 		public Vector3 Position = Vector3.zero;
 
+		public Point() {}
+
+		public Point(float x, float y, float z) {
+			Position = new Vector3(x, y, z);
+		}
+
+		public Point(Vector3 position) {
+			Position = position;
+		}
+
+		public static Geometry At(Vector3 position) {
+			var p = new Point();
+			p.Position = position;
+			return p.Output();
+		}
+
 		public Geometry Output() {
 			return new Geometry() {
 				Vertices = new Vector3[] { Position },
