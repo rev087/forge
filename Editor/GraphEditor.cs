0cf7796598d6e7eb24a2a7221b9496d124a1908d
Bruno Daniel <bruno@vault7.com>
2015-06-13 18:04:18 -0300

    Separate GraphEvent to its own file

--- Editor/GraphEditor.cs
+++ Editor/GraphEditor.cs
@@ -5,50 +5,6 @@ using System.Collections.Generic;
 
 namespace Forge.Editor {
 
-	public enum GEType { None, Unresolved, Click, Drag }
-	public enum GEContext { None, Grid, Node, Output, Input }
-	public struct GraphEvent {
-		public GEType Type;
-		public GEContext Context;
-		public Node Node;
-		public IOOutlet Outlet;
-
-		public GraphEvent(GEType type, GEContext context, Node node, IOOutlet outlet) {
-			Type = type;
-			Context = context;
-			Node = node;
-			Outlet = outlet;
-		}
-
-		public void Empty() {
-			Type = GEType.None;
-			Context = GEContext.None;
-			Node = null;
-		}
-
-		public bool IsType(params GEType[] types) {
-			foreach (GEType type in types) {
-				if (Type == type) return true;
-			}
-			return false;;
-		}
-
-		public bool IsConnecting() {
-			return Type == GEType.Drag &&
-				(Context == GEContext.Input || Context == GEContext.Output);
-		}
-
-		public bool IsNodeDrag(Node node) {
-			return Node == node && Context == GEContext.Node &&
-				IsType(GEType.Unresolved, GEType.Drag);
-		}
-
-		public bool CanDragOutlet(Node node, GEContext context) {
-			return Node == node && Context == context &&
-				IsType(GEType.Unresolved, GEType.Drag);
-		}
-	}
-
 	public class GraphEditor : EditorWindow {
 
 		private GridRenderer _gridRenderer;
