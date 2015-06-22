62eb30ddc9c20c6d0d0278bf538b7b5f665aa286
Bruno Daniel <bruno@vault7.com>
2015-06-22 03:08:37 -0300

    Add MultiplyFloat and MultoplyInteger

--- Operators/Multiply.cs
+++ Operators/Multiply.cs
@@ -2,7 +2,19 @@ using UnityEngine;
 
 namespace Forge.Operators {
 
-	public class Multiply : Operator {
+	public class MultiplyInteger : Operator {
+
+		[Input] public int A = 0;
+		[Input] public int B = 0;
+
+		[Output]
+		public int Output() {
+			return A * B;
+		}
+
+	}
+
+	public class MultiplyFloat : Operator {
 
 		[Input] public float A = 0.0f;
 		[Input] public float B = 0.0f;
