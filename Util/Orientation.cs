0bb65aa2c59d3356e047ac758f70037a29063a1d
Bruno Daniel <rev087@gmail.com>
2015-12-27 22:38:43 -0200

    Orientation is now publicly accessible. Added Orientation.Vector3()

--- Util/Orientation.cs
+++ Util/Orientation.cs
@@ -6,34 +6,43 @@ namespace Forge {
 	public enum OrientationPreset {XY, XZ, ZY};
 	public enum Axis {X=0, Y=1, Z=2};
 
-	public static class OrientationUtil {
 
-		private struct Orientation {
-			public Axis Horizontal;
-			public Axis Vertical;
-			public Axis Normal;
+	public struct Orientation {
+		public Axis Horizontal;
+		public Axis Vertical;
+		public Axis Normal;
 
-			public static Orientation Preset(OrientationPreset preset) {
-				switch (preset) {
-					case OrientationPreset.XY:
-						return new Orientation() { Horizontal = Axis.X, Vertical = Axis.Y, Normal = Axis.Z };
-					case OrientationPreset.ZY:
-						return new Orientation() { Horizontal = Axis.Z, Vertical = Axis.Y, Normal = Axis.X };
-					default:
-						return new Orientation() { Horizontal = Axis.X, Vertical = Axis.Z, Normal = Axis.Y };
-				}
-			}
-
-			public override string ToString() {
-				return System.String.Format("Horizontal:{0}, Vertical:{1}, Normal:{2}", Horizontal, Vertical, Normal);
+		public static Orientation FromPreset(OrientationPreset preset) {
+			switch (preset) {
+				case OrientationPreset.XY:
+					return new Orientation() { Horizontal = Axis.X, Vertical = Axis.Y, Normal = Axis.Z };
+				case OrientationPreset.ZY:
+					return new Orientation() { Horizontal = Axis.Z, Vertical = Axis.Y, Normal = Axis.X };
+				default:
+					return new Orientation() { Horizontal = Axis.X, Vertical = Axis.Z, Normal = Axis.Y };
 			}
 		}
+
+		public Vector3 Vector3(float horizontal, float vertical, float normal = 0f) {
+			Vector3 v = new Vector3();
+			v[(int)Horizontal] = horizontal;
+			v[(int)Vertical] = vertical;
+			v[(int)Normal] = normal;
+			return v;
+		}
+
+		public override string ToString() {
+			return System.String.Format("Horizontal:{0}, Vertical:{1}, Normal:{2}", Horizontal, Vertical, Normal);
+		}
+	}
+
+	public static class OrientationUtil {
 
 		public static void ApplyOrientation(this Geometry geo, OrientationPreset preset) {
 
 			if (preset == OrientationPreset.XZ) return;
 
-			Orientation o = Orientation.Preset(preset);
+			Orientation o = Orientation.FromPreset(preset);
 
 			for (int i = 0; i < geo.Vertices.Length; i++) {
 
@@ -51,16 +60,19 @@ namespace Forge {
 					geo.Normals[i] = normal;
 				}
 
+				if (o.Vertical != Axis.Y && i < geo.UV.Length) {
+					geo.UV[i].x = 1 - geo.UV[i].x;
+				}
+
 			}
 
 			if (o.Normal == Axis.Z) {
 				for (int t = 0; t < geo.Triangles.Length; t += 3) {
-					int a = geo.Triangles[t  ];
-					int b = geo.Triangles[t+1];
-					int c = geo.Triangles[t+2];
-					geo.Triangles[t+2] = a;
-					geo.Triangles[t+1] = b;
-					geo.Triangles[t  ] = c;
+					// b remains the same
+					int a = geo.Triangles[t];
+					int c = geo.Triangles[t + 2];
+					geo.Triangles[t + 2] = a;
+					geo.Triangles[t] = c;
 				}
 			}
 
