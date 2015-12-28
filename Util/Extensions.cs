8a1a31a02be2f01e428cbdee1b4fdd13128e3c71
Bruno Daniel <rev087@gmail.com>
2015-12-27 22:35:35 -0200

    Add int.Contains extension, tweak float.Remap extension

--- Util/Extensions.cs
+++ Util/Extensions.cs
@@ -5,11 +5,24 @@ namespace Forge.Extensions {
 
 	public static class Extensions {
 
+		public static bool Contains(this int[] array, int value) {
+			for (int i = 0; i < array.Length; i++) {
+				if (array[i] == value) return true;
+			}
+			return false;
+		}
+
 		public static Vector3 Abs(this Vector3 v) {
 			return new Vector3(Mathf.Abs(v.x), Mathf.Abs(v.y), Mathf.Abs(v.z));
 		}
 
 		public static float Remap(this float original, float fromMin, float fromMax, float toMin, float toMax) {
+			if (toMin == toMax) return toMin;
+			
+			if (fromMin == fromMax) {
+				return (toMax - toMin) / 2;
+			}
+
 			return (((original - fromMin) * (toMax - toMin)) / (fromMax - fromMin)) + toMin;
 		}
 
