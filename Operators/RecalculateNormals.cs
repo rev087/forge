bd13d6962205af41f91cf5f4732d47afb8bae820
Bruno Daniel <rev087@gmail.com>
2015-12-29 07:05:05 -0200

    Recalculate Normals operator

---
+++ Operators/RecalculateNormals.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Forge.Operators {
+
+	[OperatorMetadata(Category = "Geometry", Title = "Recalculate Normals")]
+	class RecalculateNormals : Operator {
+
+		private Geometry _geometry = Geometry.Empty;
+		[Input]
+		public void Input (Geometry input) {
+			_geometry = input;
+		}
+
+		[Output]
+		public Geometry Output() {
+			Geometry geo = _geometry.Copy();
+			geo.RecalculateNormals();
+			return geo;
+		}
+	}
+
+}
