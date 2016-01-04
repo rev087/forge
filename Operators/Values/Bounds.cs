0974058ff35b575f94cfdaa236dcb3826509aad4
Bruno Daniel <rev087@gmail.com>
2016-01-04 01:55:40 -0200

    Add Bounding Box operator

---
+++ Operators/Values/Bounds.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Forge.Operators {
+
+	[OperatorMetadata(Category = "Values", Title = "Bounding Box")]
+	class Bounds : Operator {
+
+		[Input]
+		public Geometry Input = Geometry.Empty;
+
+		[Input]
+		public Axis Axis = Axis.X;
+
+		[Output]
+		public float Min { get { return Input.Min(Axis); } }
+
+		[Output]
+		public float Max { get { return Input.Max(Axis); } }
+
+		[Output]
+		public float Span { get { return Input.Span(Axis); } }
+	}
+}
