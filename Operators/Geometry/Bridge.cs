44cc3ddd4b4344c09778be67bf8834994a110688
Bruno Daniel <rev087@gmail.com>
2016-01-04 01:57:05 -0200

    Add ClosePolygons and CloseLoop options to Bridge op

--- Operators/Geometry/Bridge.cs
+++ Operators/Geometry/Bridge.cs
@@ -7,6 +7,12 @@ namespace Forge.Operators {
 
 		[Input] public bool RecalculateNormals = false;
 
+		[Input]
+		public bool ClosePolygons = false;
+
+		[Input]
+		public bool CloseLoop = false;
+
 		private Geometry _geometry;
 
 		public Bridge() {}
@@ -49,46 +55,88 @@ namespace Forge.Operators {
 				lastIndex = _geometry.Polygons[i] + _geometry.Polygons[i + 1];
 			}
 
-			var polyLength = _geometry.Polygons.Length;
-			var polyCount = polyLength / 2;
-			var segments = GetBridgeLength(_geometry.Polygons);
-			var vertexCount = segments * polyCount;
-			var triCount = segments * (polyCount - 1) * 6;
+			var polyCount = _geometry.Polygons.Length / 2;
+			var polyLength = GetBridgeLength(_geometry.Polygons);
+			var vertexCount = polyLength * polyCount;
+			var triCount = (polyLength - 1) * (polyCount - 1) * 6;
+
+			if (ClosePolygons) {
+				triCount += (polyCount - 1) * 6;
+			}
+
+			if (CloseLoop) {
+				triCount += (polyLength - 1) * 6;
+
+				if (ClosePolygons) {
+					triCount += 6;
+				}
+			}
 
 			var geo = new Geometry(vertexCount, triCount, polyCount);
 
 			int vCount = 0;
 			int tCount = 0;
 
-			for (var p = 0; p < polyLength; p+=2) {
+			for (var p = 0; p < _geometry.Polygons.Length; p+=2) {
 				var start = _geometry.Polygons[p];
 				
-				if (_geometry.Polygons[p+1] != segments) {
-					Debug.LogErrorFormat("Bridge error: input polygons have different numbers of vertices\nGot {0}, expected {1}", _geometry.Polygons[p+1], segments);
+				if (_geometry.Polygons[p+1] != polyLength) {
+					Debug.LogErrorFormat("Bridge error: input polygons have different numbers of vertices\nGot {0}, expected {1}", _geometry.Polygons[p+1], polyLength);
 					return Geometry.Empty;
 				}
 
-				for (var v = start; v < start+segments; v++) {
-					//Debug.Log(v);
+				for (var v = start; v < start + polyLength; v++) {
 					geo.Vertices[vCount] = _geometry.Vertices[v];
-					if (v < _geometry.Normals.Length) geo.Normals[vCount] = _geometry.Normals[v];
-					if (v < _geometry.Tangents.Length) geo.Tangents[vCount] = _geometry.Tangents[v];
-					if (v < _geometry.UV.Length) geo.UV[vCount] = _geometry.UV[v];
+					geo.Normals[vCount] = _geometry.Normals[v];
+					geo.Tangents[vCount] = _geometry.Tangents[v];
+					geo.UV[vCount] = _geometry.UV[v];
 					vCount++;
 
-					if (p > 0) {
-						geo.Triangles[tCount++] = v;
-						geo.Triangles[tCount++] = v-segments;
-						geo.Triangles[tCount++] = (v-segments+1 == start) ? start-segments : v-segments+1;
-						
-						geo.Triangles[tCount++] = (v-1 < start) ? start+segments-1 : v-1;
-						geo.Triangles[tCount++] = v-segments;
-						geo.Triangles[tCount++] = v;
-
-						//Debug.LogFormat("tri: {0} {1} {2}", v, v-segments, (v - segments + 1 == start) ? start - segments : v - segments + 1, geo.Vertices.Length - 1);
-						//Debug.LogFormat("tri: {0} {1} {2}", (v - 1 < start) ? start + segments - 1 : v - 1, v - segments, v);
+					// Skip the last vertex of each polygon if ClosePolygons is fale
+					if (v == start + polyLength - 1 && !ClosePolygons) {
+						continue;
 					}
 
+					// 2nd to nth polygons require no special cases
+					if (p > 0) {
+						// Lower-right triangle
+						geo.Triangles[tCount++] = v;
+						geo.Triangles[tCount++] = v - polyLength;
+						geo.Triangles[tCount++] = (v - polyLength + 1 == start) ? start - polyLength : v - polyLength + 1;
+
+						// Upper-left triangle
+						geo.Triangles[tCount++] = (v - polyLength + 1 == start) ? start - polyLength : v - polyLength + 1;
+						geo.Triangles[tCount++] = v + 1 < start + polyLength ? v + 1 : start;
+						geo.Triangles[tCount++] = v;
+					}
+
+					// First polygon is skipped if CloseLoop is false
+					else if (p == 0 && CloseLoop) {
+
+						// The last vertex of the first polygon
+						if (v == polyLength - 1 && ClosePolygons) {
+							geo.Triangles[tCount++] = v;
+							geo.Triangles[tCount++] = vertexCount - 1;
+							geo.Triangles[tCount++] = vertexCount - polyLength;
+							Debug.LogFormat("{0}: {1}, {2}, {3}", v, geo.Triangles[tCount - 3], geo.Triangles[tCount - 2], geo.Triangles[tCount - 1]);
+
+							geo.Triangles[tCount++] = vertexCount - polyLength;
+							geo.Triangles[tCount++] = 0;
+							geo.Triangles[tCount++] = v;
+							Debug.LogFormat("{0}: {1}, {2}, {3}", v, geo.Triangles[tCount - 3], geo.Triangles[tCount - 2], geo.Triangles[tCount - 1]);
+						}
+
+						// The first to second-to-last vertices of the first polygon
+						else {
+							geo.Triangles[tCount++] = v;
+							geo.Triangles[tCount++] = polyCount * polyLength - polyLength + v;
+							geo.Triangles[tCount++] = polyCount * polyLength - polyLength + v + 1;
+
+							geo.Triangles[tCount++] = polyCount * polyLength - polyLength + v + 1;
+							geo.Triangles[tCount++] = v + 1;
+							geo.Triangles[tCount++] = v;
+						}
+					}
 				}
 			}
 
