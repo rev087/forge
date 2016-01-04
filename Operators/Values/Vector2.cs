f3befdb7a986ec4451e941194be4e6487a6d5a6e
Bruno Daniel <rev087@gmail.com>
2016-01-04 01:56:03 -0200

    Add Vector2 operators

---
+++ Operators/Values/Vector2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Forge.Operators {
+
+	[OperatorMetadata(Category = "Values", Title = "XY to Vector2")]
+	class XYZToVector2 : Operator {
+		[Input]
+		public float X = 0f;
+		[Input]
+		public float Y = 0f;
+
+		[Output]
+		public Vector2 Vector2() {
+			return new Vector2(X, Y);
+		}
+	}
+
+	[OperatorMetadata(Category = "Values", Title = "Vector2 to XY")]
+	class Vector2ToXYZ : Operator {
+		[Input]
+		public Vector2 Vector = Vector2.zero;
+		[Output]
+		public float X { get { return Vector.x; } }
+		[Output]
+		public float Y { get { return Vector.y; } }
+	}
+
+}
