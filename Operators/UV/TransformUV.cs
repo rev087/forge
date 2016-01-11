589b9cb5c31f1ac3c0b82409957f4b34f18a5868
Bruno Daniel <rev087@gmail.com>
2016-01-11 01:58:57 -0200

    Add Transform UV operator (rotation does not work yet)

---
+++ Operators/UV/TransformUV.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Forge.Operators {
+
+	[OperatorMetadata(Category = "UV", Title = "Transform UV")]
+	public class TransformUV : Operator {
+
+		[Input]
+		public Geometry Input = Geometry.Empty;
+		[Input]
+		public Vector2 Position = Vector2.zero;
+		[Input]
+		public Vector2 Rotation = Vector2.zero;
+		[Input]
+		public Vector2 Scale = Vector2.one;
+
+		[Output]
+		public Geometry Output() {
+			Geometry geo = Input.Copy();
+
+			if (Position != Vector2.zero) {
+				for (int i = 0; i < geo.UV.Length; i++) {
+					geo.UV[i] = geo.UV[i] + Position;
+				}
+			}
+
+			if (Rotation != Vector2.zero) {
+				Quaternion qRot = Quaternion.Euler(Rotation);
+				for (int i = 0; i < geo.UV.Length; i++) {
+					geo.UV[i] = qRot * geo.UV[i];
+				}
+			}
+
+			if (Scale != Vector2.one) {
+				for (int i = 0; i < geo.UV.Length; i++) {
+					geo.UV[i] = Vector2.Scale(geo.UV[i], Scale);
+				}
+			}
+
+			return geo;
+		}
+
+	}
+
+}
