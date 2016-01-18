4a6be1307ed8266455bc22a9541f9ae848cc25ab
Bruno Daniel <rev087@gmail.com>
2016-01-18 01:48:26 -0200

    Add VertexNormals operator

---
+++ Operators/Geometry/VertexNormals.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Forge.Operators {
+
+	[OperatorMetadata(Category = "Geometry", Title = "Vertex Normals")]
+	class VertexNormals : Operator {
+
+		[Input]
+		public Geometry Input = Geometry.Empty;
+
+		[Input]
+		public Vector3 Normal = Vector3.zero;
+
+		[Input]
+		public Vector4 Tangent = Vector4.zero;
+
+		[Output]
+		public Geometry Output() {
+			Geometry geo = Input.Copy();
+
+			Vector3[] normals = new Vector3[geo.Vertices.Length];
+			Vector4[] tangents = new Vector4[geo.Vertices.Length];
+
+			for (int i = 0; i < geo.Vertices.Length; i++) {
+				normals[i] = Normal;
+				tangents[i] = Tangent;
+			}
+
+			geo.Normals = normals;
+			geo.Tangents = tangents;
+
+			return geo;
+		}
+	}
+}
