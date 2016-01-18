2ca3907eb75ac65d90473ed49ee59ce07f783314
Bruno Daniel <rev087@gmail.com>
2016-01-18 01:47:47 -0200

    Add IntegerInput

---
+++ Operators/Inputs/IntegerInput.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Forge.Operators {
+
+	[OperatorMetadata(Category = "Input", Title = "Integer", DataType = typeof(float))]
+	public class IntegerInput : Parameter {
+
+		[Input]
+		public int Min = 0;
+
+		[Input]
+		public int Max = 100;
+
+		[Input]
+		public int Default = 0;
+
+		[Input]
+		public bool DisplaySlider = true;
+
+		[Output]
+		[ParameterInput]
+		public int Integer = 0;
+
+#if UNITY_EDITOR
+		[ParameterGUI]
+		public object InputGUI(object intValue) {
+			int value = intValue == null ? Default : (int)intValue;
+			if (DisplaySlider) {
+				Integer = (int) EditorGUILayout.Slider(Label, value, Min, Max);
+			} else {
+				Integer = (int)EditorGUILayout.IntField(Label, value);
+			}
+			return Integer;
+		}
+#endif
+	}
+
+}
