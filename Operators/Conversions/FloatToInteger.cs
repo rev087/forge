af2285fb84b121187e4cbf17c9431ea75c910ced
Bruno Daniel <rev087@gmail.com>
2016-01-18 01:48:11 -0200

    Add FloatToInteger and IntegerToFloat conversion operators

---
+++ Operators/Conversions/FloatToInteger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Forge.Operators {
+
+	[OperatorMetadata(Category = "Conversions", Title = "Float to Integer")]
+	public class FloatToInteger : Operator {
+
+		[Input]
+		public float Float = 0f;
+
+		[Output]
+		public int Integer {
+			get {
+				return (int) Float;
+			}
+		}
+	}
+
+}
---
+++ Operators/Conversions/IntegerToFloat.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Forge.Operators {
+
+	[OperatorMetadata(Category = "Conversions", Title = "Integer to Float")]
+	public class IntegerToFloat : Operator {
+
+		[Input]
+		public int Integer = 0;
+
+		[Output]
+		public float Float {
+			get {
+				return Integer;
+			}
+		}
+	}
+
+}
