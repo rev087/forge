7dbb044b902f5a1c90fd2aac3f2052c5197fd196
Bruno Daniel <rev087@gmail.com>
2016-01-18 04:37:54 -0200

    Add Align operator

---
+++ Operators/Geometry/Align.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Forge.Operators{
+
+	[OperatorMetadata(Category = "Geometry")]
+	public class Align : Operator {
+
+		public enum AlignmentType { Min, Max, Center }
+
+		[Input]
+		public Geometry Input = Geometry.Empty;
+
+		[Input]
+		public Axis Axis = Axis.X;
+
+		[Input]
+		public AlignmentType AlignTo = AlignmentType.Min;
+
+		[Input]
+		public float Value = 0f;
+
+		[Output]
+		public Geometry Output() {
+			var transf = new TransformGeometry();
+			transf.Input(Input);
+			transf.Position = Vector3.zero;
+
+			switch (AlignTo) {
+				case AlignmentType.Min:
+					transf.Position[(int)Axis] = Value - Input.Min(Axis);
+					break;
+				case AlignmentType.Max:
+					transf.Position[(int)Axis] = - (Input.Max(Axis) - Value);
+					break;
+				case AlignmentType.Center:
+					transf.Position[(int)Axis] = (Value - Input.Span(Axis)) / - 2f;
+					Debug.Log(transf.Position);
+					break;
+			}
+
+			return transf.Output();
+		}
+
+	}
+
+}
